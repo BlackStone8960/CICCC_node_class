571c76101ae56892e57db2eac24e369c798f0dd3
Taichi I <fng.trf.kk1020@gmail.com>
2021-08-11 03:22:27 -0700

    fixed a little

--- regular-expression/regex.js
+++ regular-expression/regex.js
@@ -1,6 +1,8 @@
+// This file will return "Valid password" if password provided as argument in CLI is matched with the condition, and return "Not a valid password" if it is not matched.
+
 const validatePassword = (password) => {
   const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,}$/;
-  return regex.test(password);
+  return regex.test(password) ? "Valid password" : "Not a valid password";
 };
 
 console.log(validatePassword(process.argv[2]));
